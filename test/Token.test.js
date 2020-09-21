eca6173eb90d3170546db70db0968fe0c8e52bab
Pablo Ocaña <pabloocana@MacBook-Pro-de-Pablo.local>
2020-09-21 23:39:41 +0200

    changed solidity compiler, added decimals/total supply tests

--- test/Token.test.js
+++ test/Token.test.js
@@ -8,17 +8,34 @@ const Token = artifacts.require("./Token");
 chai.use(chaiAsPromised).should();
 
 contract("Token", (account) => {
+
+    let token;
     const symbol = "QTKN";
+    const decimals = "18";
+    const totalSupply = "1000000000000000000000000";
+
+
+    beforeEach(async () => {
+        token = await Token.new();
+    })
+
+    
     describe("deployment", () => {
         it("Should have name QTOKEN", async () => {
-            const token = await Token.new();
             const result = await token.name();
             result.should.equal("QTOKEN");
         });
-        it('tracks the symbol', async ()  => {
-            const token = await Token.new();
-            const result = await token.symbol()
-            result.should.equal(symbol)
+        it("tracks the symbol", async ()  => {
+            const result = await token.symbol();
+            result.should.equal(symbol);
+        });
+        it("tracks the decimals", async () => {
+            const result = await token.decimals();
+            result.toString().should.equal(decimals);
+        });
+        it("tracks the total supply", async () => {
+            const result = await token.totalSupply();
+            result.toString().should.equal(totalSupply);
         });
     });
 });
